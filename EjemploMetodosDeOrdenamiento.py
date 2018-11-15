da3fe2ac49859cc9f0a5f751b679a2e6002bf307
Cristofer Casas <cristofer32513@gmail.com>
2018-11-15 00:07:36 -0600

    Se agrego el metodo ordenamientoBurbuja0.

--- EjemploMetodosDeOrdenamiento.py
+++ EjemploMetodosDeOrdenamiento.py
@@ -17,5 +17,28 @@ class MetodosDeOrdenamiento:
         print("    - Cantidad de intercambios realizados: "+str(contadorIntercambios))
         print("    - Tiempo total de ejecucion: "+str(float(tiempoTotal)/1000000)+" milisegundos")
     
+    def ordenamientoBurbuja0(self, datos):
+        contadorComparaciones=0, contadorIntercambios=0, contadorRecorridos=0
+        tiempoTotal=0, tiempoInicial=0
+        
+        aux=0
+        #tiempoInicial=System.nanoTime()
+        for i in range(0, len(datos)):
+            for j in range(i+1, len(datos)):
+                contadorComparaciones+=1
+                if(datos[i]>datos[(j)]):
+                    contadorIntercambios+=1
+                    aux=datos[i]
+                    datos[i]=datos[j]
+                    datos[j]=aux
+                
+            
+            contadorRecorridos+=1
+        #tiempoTotal=System.nanoTime()-tiempoInicial
+        self.__mostrarVector(datos)
+        print()
+        print()
+        self.__mostrarDatosDeEficiencia(contadorComparaciones, contadorIntercambios, contadorRecorridos, tiempoTotal);
+    
         
     
