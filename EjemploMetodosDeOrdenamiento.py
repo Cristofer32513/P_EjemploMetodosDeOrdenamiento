829d43191ccad29534cbcdf33f6ae6cadc00ad5e
Cristofer Casas <cristofer32513@gmail.com>
2018-11-14 23:49:33 -0600

    Se agrego el metodo mostrarDatosDeEficiencia.

--- EjemploMetodosDeOrdenamiento.py
+++ EjemploMetodosDeOrdenamiento.py
@@ -8,4 +8,14 @@ class MetodosDeOrdenamiento:
     def mostrarVector(self, datos):
         print(datos)
     
+    def mostrarDatosDeEficiencia(self, contadorComparaciones, contadorIntercambios,
+                                 contadorRecorridos, tiempoTotal):
+        print("       DATOS DE EFICIENCIA DEL ALGORITMO");
+        print();
+        print("    - Cantidad de recorridos realizados: "+str(contadorRecorridos))
+        print("    - Cantidad de comparaciones realizadas: "+str(contadorComparaciones))
+        print("    - Cantidad de intercambios realizados: "+str(contadorIntercambios))
+        print("    - Tiempo total de ejecucion: "+str(float(tiempoTotal)/1000000)+" milisegundos")
+    
+        
     
