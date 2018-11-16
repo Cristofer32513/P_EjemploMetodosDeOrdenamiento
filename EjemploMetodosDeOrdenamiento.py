42c63f9410d6f932efb71b94418bb5d2c1b2f8c7
Cristofer Casas <cristofer32513@gmail.com>
2018-11-16 17:03:13 -0600

    Se modifico el metodo mostrarDatosDeEficiencia.

--- EjemploMetodosDeOrdenamiento.py
+++ EjemploMetodosDeOrdenamiento.py
@@ -13,10 +13,11 @@ class MetodosDeOrdenamiento:
                                  contadorRecorridos, tiempoTotal):
         print("       DATOS DE EFICIENCIA DEL ALGORITMO");
         print();
-        print("    - Cantidad de recorridos realizados: "+str(contadorRecorridos))
-        print("    - Cantidad de comparaciones realizadas: "+str(contadorComparaciones))
-        print("    - Cantidad de intercambios realizados: "+str(contadorIntercambios))
-        print("    - Tiempo total de ejecucion: "+str(tiempoTotal)+" segundos")
+        print("    - Cantidad  de  recorridos  realizados:    "+str(contadorRecorridos))
+        print("    - Cantidad de comparaciones realizadas:    "+str(contadorComparaciones))
+        print("    - Cantidad  de intercambios realizados:    "+str(contadorIntercambios))
+        print("    - Tiempo     total     de    ejecucion:    "+str(tiempoTotal)+" segundos")
+        print("    - Tiempo     total     de    ejecucion:    "+str(tiempoTotal*1000)+" milisegundos")
     
     def ordenamientoBurbuja0(self, datos):
         contadorComparaciones=0
