3da28ac46076d80f992f50fb0a69db43a2e1623e
Cristofer Casas <cristofer32513@gmail.com>
2018-11-28 20:20:42 -0600

    Se agrego el metodo ordenamientoPorIntercalaion.

--- EjemploMetodosDeOrdenamiento.py
+++ EjemploMetodosDeOrdenamiento.py
@@ -314,8 +314,67 @@ class MetodosDeOrdenamiento:
             new_list = self.merge(self.split(new_list, base, digit_num))
         return self.merge(self.split_by_sign(new_list))   
             
-     
-     
+
+    '''========METODO DE ORDENAMIENTO POR INTERCALACION========='''
+    def ordenamientoPorIntercalaion(self):
+        archivo3=open ("ArchivoSalida.txt", "w")
+        archivo1=open("Archivo1.txt", "r")
+        archivo2=open("Archivo2.txt", "r")
+        repetir=True
+          
+        lineaArchivo1=archivo1.readline() 
+        lineaArchivo2=archivo2.readline()
+         
+        
+        '''Se realizan comparaciones mientras la bandera no cambie'''
+        while(repetir):
+            if(int(lineaArchivo1)<int(lineaArchivo2)):
+                archivo3.write(lineaArchivo1)
+                lineaArchivo1=archivo1.readline()
+                if(lineaArchivo1==""):
+                    archivo3.write("\n")
+                    archivo3.write(lineaArchivo2)
+                    lineaArchivo2=archivo2.readline()
+                    while(lineaArchivo2!=""):
+                        archivo3.write(lineaArchivo2)
+                        lineaArchivo2=archivo2.readline()
+                    repetir=False
+            elif(int(lineaArchivo1)>int(lineaArchivo2)):
+                archivo3.write(lineaArchivo2)
+                lineaArchivo2=archivo2.readline()
+                if(lineaArchivo2==""):
+                    archivo3.write("\n")
+                    archivo3.write(lineaArchivo1)
+                    lineaArchivo1=archivo1.readline()
+                    while(lineaArchivo1!=""):
+                        archivo3.write(lineaArchivo1)
+                        lineaArchivo1=archivo1.readline()
+                    repetir=False
+            else:
+                archivo3.write(lineaArchivo1)
+                archivo3.write(lineaArchivo2)
+                lineaArchivo1=archivo1.readline()
+                if(lineaArchivo1==""):
+                    archivo3.write("\n")
+                    archivo3.write(lineaArchivo2)
+                    lineaArchivo2=archivo2.readline()
+                    while(lineaArchivo2!=""):
+                        archivo3.write(lineaArchivo2)
+                        lineaArchivo2=archivo2.readline()
+                    repetir=False
+                lineaArchivo2=archivo2.readline()
+                if(lineaArchivo2==""):
+                    archivo3.write("\n")
+                    archivo3.write(lineaArchivo1)
+                    lineaArchivo1=archivo1.readline()
+                    while(lineaArchivo1!=''):
+                        archivo3.write(lineaArchivo1)
+                        lineaArchivo1=archivo1.readline()
+                    repetir=False
+        archivo2.close
+        archivo1.close
+        print("Archivos combinados y ordenados correctamente")
+        archivo3.close
        
 metodos=MetodosDeOrdenamiento()
                        
